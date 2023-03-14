ff04f8362a77579d3f54ba639a9f1e6af8e26422
KaterinaD3 <96959402+KaterinaD3@users.noreply.github.com>
2023-03-14 13:51:43 +0200

    Add files via upload

--- set.h
+++ set.h
@@ -1,26 +1,31 @@
-//
-// Created by Katerina Dimitrova on 7.03.23.
-//
-
-#ifndef OOP_SET_H
-#define OOP_SET_H
-
-#endif //OOP_SET_H
-
-
 class Set {
-
-  int* elements;
+    int* elements;
 
     int numElements;
     int capacity;
 
     void resize();
+    void copy(int const* elements, int numElements, int capacity);
 
 public:
     Set();
     Set(Set const& other);
     Set& operator= (Set const& other);
+    Set& operator+ (Set const& other);
+    Set& operator+= (Set const& other);
+   // Set& operator+ (int number, Set const& other);
+    Set& operator* (Set const& other);
+    Set& operator/ (Set const& other);
+    Set& operator/= (Set const& other);
+    Set& operator% (Set const& other);
+    Set& operator%= (Set const& other);
+    Set& operator== (Set const& other);
+    Set& operator!= (Set const& other);
+    Set& operator< (Set const& other);
+    Set& operator<= (Set const& other);
+    Set& operator>= (Set const& other);
+    Set& operator[] (Set const& other);
+
     ~Set();
 
     bool addElement(int const element);
@@ -28,6 +33,4 @@ public:
     void setUnion(Set const& other);
     void setIntersection(Set const& other);
     void print();
-    void copy (int const* elements, int numElements, int capacity);
-
 };
